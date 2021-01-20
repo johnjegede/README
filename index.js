b6683fb7e1313f92e733490356a5c376e9aae178
John Jegede <jegede400@gmail.com>
2021-01-20 09:17:20 -0500

    added the questions

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+const util = require('util');
+
+
+
 // array of questions for user
 const questions = [
+    "What is the title of your project",
+    "What is the description of your project",
+    "What are the installation instructions",
+    "What is the usage information",
+    "What are the contribution guidelines",
+    "What are the test instructions"
 
 ];
 
